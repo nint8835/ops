861325de35a84d47aa6accce7bdb1e64a94e1761
Riley Flynn <riley@rileyflynn.me>
2023-05-31 20:59:28 -0230

    Configure cert-manager (untested)

---
+++ terraform/cert-manager.tf
@@ -0,0 +1,76 @@
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  repository = "https://charts.jetstack.io"
+
+  chart     = "cert-manager"
+  name      = "cert-manager"
+  namespace = kubernetes_namespace.cert_manager.id
+
+  set {
+    name  = "installCRDs"
+    value = true
+  }
+}
+
+resource "kubernetes_manifest" "letsencrypt_staging_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "letsencrypt-staging"
+    }
+
+    spec = {
+      acme = {
+        email  = "riley@rileyflynn.me"
+        server = "https://acme-staging-v02.api.letsencrypt.org/directory"
+        privateKeySecretRef = {
+          name = "letsencrypt-staging-account-key"
+        }
+        solvers = [
+          {
+            http01 = {
+              ingress = {
+                ingressClassName = "traefik"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "letsencrypt_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "letsencrypt"
+    }
+
+    spec = {
+      acme = {
+        email  = "riley@rileyflynn.me"
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+        privateKeySecretRef = {
+          name = "letsencrypt-account-key"
+        }
+        solvers = [
+          {
+            http01 = {
+              ingress = {
+                ingressClassName = "traefik"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+}
