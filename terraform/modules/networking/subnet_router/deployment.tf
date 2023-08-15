8280ea863a21b0fe1b1b72b3277be5960dbc1e06
Riley Flynn <riley@rileyflynn.me>
2023-08-15 18:29:14 -0230

    Migrate router from pod to deployment

---
+++ terraform/modules/networking/subnet_router/deployment.tf
@@ -0,0 +1,72 @@
+resource "kubernetes_deployment" "router" {
+  metadata {
+    name      = var.router_name
+    namespace = var.namespace
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app   = "tailscale"
+        route = var.router_name
+      }
+    }
+
+    replicas = 1
+
+    template {
+      metadata {
+        labels = {
+          app   = "tailscale"
+          route = var.router_name
+        }
+      }
+
+      spec {
+        service_account_name = kubernetes_service_account.service_account.metadata[0].name
+
+        container {
+          name              = "tailscale"
+          image_pull_policy = "Always"
+          image             = "ghcr.io/tailscale/tailscale:latest"
+
+          env {
+            name  = "TS_KUBE_SECRET"
+            value = kubernetes_secret.state.metadata[0].name
+          }
+
+          env {
+            name  = "TS_USERSPACE"
+            value = "true"
+          }
+
+          env {
+            name = "TS_AUTHKEY"
+            value_from {
+              secret_key_ref {
+                name     = var.ts_auth_key_secret
+                key      = "TS_AUTHKEY"
+                optional = true
+              }
+            }
+          }
+
+          env {
+            name  = "TS_ROUTES"
+            value = join(",", var.routes)
+          }
+
+          env {
+            name  = "TS_HOSTNAME"
+            value = var.router_name
+          }
+        }
+
+        security_context {
+          run_as_user  = 1000
+          run_as_group = 1000
+        }
+      }
+    }
+  }
+}
--- terraform/modules/networking/subnet_router/route_acceptance.tf
+++ terraform/modules/networking/subnet_router/route_acceptance.tf
@@ -1,12 +1,10 @@
 data "tailscale_devices" "router" {
-  depends_on = [kubernetes_pod.router]
+  depends_on = [kubernetes_deployment.router]
 
   name_prefix = var.router_name
 }
 
 resource "tailscale_device_subnet_routes" "service_router" {
-  for_each = toset([for device in data.tailscale_devices.router.devices : device.id])
-
-  device_id = each.key
+  device_id = data.tailscale_devices.router.devices[0].id
   routes    = var.routes
 }
