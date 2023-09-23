e2c3b5061305014c4e611a9450ee5aa4fda2dfae
Riley Flynn <riley@rileyflynn.me>
2023-09-23 19:43:31 -0230

    Deploy cloudnative-pg

---
+++ terraform/postgres.tf
@@ -0,0 +1,12 @@
+resource "kubernetes_namespace" "cnpg_system" {
+  metadata {
+    name = "cnpg-system"
+  }
+}
+
+resource "helm_release" "cloudnativepg" {
+  repository = "https://cloudnative-pg.github.io/charts"
+  name       = "cnpg"
+  chart      = "cloudnative-pg"
+  namespace  = kubernetes_namespace.cnpg_system.metadata.0.name
+}
