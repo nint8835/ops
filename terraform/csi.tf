638761e168ba15169a7a1891b30f4a4b569c3dde
Riley Flynn <riley@rileyflynn.me>
2024-01-16 18:57:26 -0330

    Uninstall subdir provisioner

--- terraform/csi.tf
+++ terraform/csi.tf
@@ -1,21 +1,3 @@
-resource "helm_release" "nfs_subdir_external_provisioner" {
-  repository = "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/"
-
-  name      = "nfs-subdir-external-provisioner"
-  chart     = "nfs-subdir-external-provisioner"
-  namespace = "kube-system"
-
-  set {
-    name  = "nfs.server"
-    value = "192.168.1.210"
-  }
-
-  set {
-    name  = "nfs.path"
-    value = "/volume1/Kubernetes PVs"
-  }
-}
-
 resource "helm_release" "csi_nfs" {
   repository = "https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts"
 
