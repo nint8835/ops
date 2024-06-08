4f392ed01284a9052f59a0c2828adf1a818988e0
Riley Flynn <riley@rileyflynn.me>
2024-06-08 13:35:49 -0230

    Create DNS record to resolve all proxmox hosts

--- terraform/modules/networking/proxmox_host/dns.tf
+++ terraform/modules/networking/proxmox_host/dns.tf
@@ -5,3 +5,11 @@ resource "cloudflare_record" "host" {
   type  = "A"
   value = var.ip
 }
+
+resource "cloudflare_record" "host_lb" {
+  zone_id = var.zone_id
+
+  name  = var.subdomain
+  type  = "A"
+  value = var.ip
+}
