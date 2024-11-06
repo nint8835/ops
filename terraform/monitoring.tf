bb250d0761597796d6b02ea864ca575a32c4da8d
Riley Flynn <riley@rileyflynn.me>
2024-11-06 16:59:19 -0330

    Upgrade loki

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -75,7 +75,7 @@ resource "helm_release" "loki" {
 
   repository = "https://grafana.github.io/helm-charts"
   chart      = "loki"
-  version    = "6.10.0"
+  version    = "6.18.0"
 
   values = [
     <<EOF
