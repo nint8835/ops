46b49fc4219964b85f2f747cc8f8b4251ee2a35f
Riley Flynn <riley@rileyflynn.me>
2025-02-16 13:44:20 -0330

    Deploy Interruption Spotter

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -47,3 +47,10 @@ resource "cloudflare_record" "scribe" {
   content = cloudflare_record.bastion.hostname
   type    = "CNAME"
 }
+
+resource "cloudflare_record" "interruption_spotter" {
+  zone_id = data.cloudflare_zone.bootleg_technology.zone_id
+  name    = "interruption-spotter"
+  content = cloudflare_record.bastion.hostname
+  type    = "CNAME"
+}
