158da2e34874003a47b62fbc07678d72b8261fb5
Riley Flynn <riley@rileyflynn.me>
2025-04-27 12:57:45 -0230

    Upgrade cert-manager

--- terraform/cert-manager.tf
+++ terraform/cert-manager.tf
@@ -21,7 +21,7 @@ resource "helm_release" "cert_manager" {
 
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = "v1.17.1"
+  version    = "v1.17.2"
 
   set {
     name  = "installCRDs"
