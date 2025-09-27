0bc24961ec7abcb87ae4fab9cd5c9a6ec261743c
Riley Flynn <riley@rileyflynn.me>
2025-09-27 19:19:04 -0230

    DNS cleanup pt 2: combine all ingress domain records to one resource definition

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -3,123 +3,38 @@ data "cloudflare_zones" "zones" {}
 
 locals {
   zone_ids = { for z in data.cloudflare_zones.zones.result : z.name => z.id }
+
+  ingress_domains = [
+    "calibre.bootleg.technology",
+    "flux-webhook-receiver.ops.bootleg.technology",
+    "grafana.ops.bootleg.technology",
+    "homeassistant.bootleg.technology",
+    "interruption-spotter.bootleg.technology",
+    "miniflux.bootleg.technology",
+    "paperless.bootleg.technology",
+    "photos.rileyflynn.me",
+    "pkg.nit.so",
+    "plex.bootleg.technology",
+    "pollster.bootleg.technology",
+    "scribe.bootleg.technology",
+    "shopkeeper.bootleg.technology",
+    "skyline.bootleg.technology",
+    "traefik.ops.bootleg.technology",
+  ]
+
+  ingress_entries = {
+    for domain in local.ingress_domains : domain => {
+      zone_id = local.zone_ids[regex("\\.(\\w+\\.\\w+)$", domain)[0]]
+      name    = domain
+    }
+  }
 }
 
-resource "cloudflare_dns_record" "pkg_nit_so" {
-  zone_id = local.zone_ids["nit.so"]
-  name    = "pkg.nit.so"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
+resource "cloudflare_dns_record" "ingress" {
+  for_each = local.ingress_entries
 
-resource "cloudflare_dns_record" "miniflux" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "miniflux.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "shopkeeper" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "shopkeeper.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "homeassistant" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "homeassistant.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "skyline" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "skyline.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "scribe" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "scribe.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "interruption_spotter" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "interruption-spotter.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "calibre" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "calibre.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "pollster" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "pollster.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "plex" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "plex.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "flux_webhook_receiver" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "flux-webhook-receiver.ops.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "photos" {
-  zone_id = local.zone_ids["rileyflynn.me"]
-  name    = "photos.rileyflynn.me"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "paperless" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "paperless.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "grafana" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "grafana.ops.bootleg.technology"
-  content = local.bastion_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "traefik_dashboard" {
-  zone_id = local.zone_ids["bootleg.technology"]
-  name    = "traefik.ops.bootleg.technology"
+  zone_id = each.value.zone_id
+  name    = each.value.name
   content = local.bastion_hostname
   type    = "CNAME"
   ttl     = 1
