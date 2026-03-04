3113f37c8ce7f1eb7ad7270a7ea9f4b5e6e5ed38
Riley Flynn <riley@rileyflynn.me>
2026-03-03 20:57:23 -0330

    Disable VS Code button, deploy VS Code Web

--- terraform/modules/coder/templates/docker/src/coder.tf
+++ terraform/modules/coder/templates/docker/src/coder.tf
@@ -1,6 +1,10 @@
 resource "coder_agent" "agent" {
   arch = data.coder_provisioner.me.arch
   os   = "linux"
+
+  display_apps {
+    vscode = false
+  }
 }
 
 module "git_config" {
@@ -11,4 +15,14 @@ module "git_config" {
 
   agent_id              = coder_agent.agent.id
   allow_username_change = false
+}
+
+module "vscode-web" {
+  count = data.coder_workspace.me.start_count
+
+  source  = "registry.coder.com/coder/vscode-web/coder"
+  version = "1.5.0"
+
+  agent_id       = coder_agent.agent.id
+  accept_license = true
 }
