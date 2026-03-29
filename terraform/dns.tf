f0863f92c0d6b1525e49d026bf07f2538e1baf87
Riley Flynn <riley@rileyflynn.me>
2026-03-29 14:19:26 +0200

    Undeploy Ergo

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -10,7 +10,6 @@ locals {
     "grafana.ops.bootleg.technology",
     "homeassistant.bootleg.technology",
     "interruption-spotter.bootleg.technology",
-    "irc.bootleg.technology",
     "llm.ops.bootleg.technology",
     "miniflux.bootleg.technology",
     "netbox.ops.bootleg.technology",
