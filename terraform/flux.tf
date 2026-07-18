0462f37ada1475eb5f424d8d3f3d2bf4d6079bac
Riley Flynn <riley@rileyflynn.me>
2026-07-17 21:33:58 -0230

    Update flux

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -14,7 +14,7 @@ resource "flux_bootstrap_git" "bootstrap" {
   depends_on = [github_repository_deploy_key.flux]
 
   path    = "flux"
-  version = "v2.9.1"
+  version = "v2.9.2"
   components_extra = [
     "image-reflector-controller",
     "image-automation-controller",
